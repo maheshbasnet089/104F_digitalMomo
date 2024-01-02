68a208b6e389a0f8bab69a64fbbbcdb84547cd71
Manish Basnet <manishbasnet@Manishs-MacBook-Air.local>
2024-01-02 22:32:43 +0545

    fix some apis

--- controller/admin/order/orderController.js
+++ controller/admin/order/orderController.js
@@ -1,4 +1,5 @@
 const Order = require("../../../model/orderSchema")
+const Product = require("../../../model/productModel")
 
 exports.getAllOrders = async(req,res)=>{
   
@@ -50,6 +51,54 @@ exports.updateOrderStatus = async(req,res)=>{
     }
     const updatedOrder = await Order.findByIdAndUpdate(id,{
         orderStatus
+    },{new:true}).populate({
+        path:"items.product",
+        model : "Product"
+    }).populate('user')
+let necessaryData
+    if(orderStatus === "delivered"){
+         necessaryData = updatedOrder.items.map((item)=>{
+            return {
+                quantity : item.quantity,
+                productId : item.product._id,
+                productStockQty : item.product.productStockQty
+            }
+        })
+
+        for(var i = 0 ; i < necessaryData.length; i ++){
+            await Product.findByIdAndUpdate(necessaryData[i].productId,{
+                productStockQty : necessaryData[i].productStockQty - necessaryData[i].quantity
+            })
+        }
+
+
+    }
+
+
+    res.status(200).json({
+        message : "Order status updated Successfully",
+        data : updatedOrder
+    })
+}
+
+
+exports.updatePaymentStatus = async(req,res)=>{
+    const {id} = req.params 
+    const {paymentStatus} = req.body 
+
+    if(!paymentStatus || !['pending','unpaid','paid'].includes(paymentStatus.toLowerCase())){
+        return res.status(400).json({
+            message : "paymentStatus is invalid or should be provided"
+        })
+    }
+    const order= await Order.findById(id)
+    if(!order){
+        return res.status(404).json({
+            message : "No order found with that id"
+        })
+    }
+    const updatedOrder = await Order.findByIdAndUpdate(id,{
+        'paymentDetails.status' : paymentStatus
     },{new:true}).populate({
         path:"items.product",
         model : "Product"
--- routes/admin/adminOrderRoute.js
+++ routes/admin/adminOrderRoute.js
@@ -1,4 +1,4 @@
-const { getAllOrders, getSingleOrder, updateOrderStatus, deleteOrder } = require("../../controller/admin/order/orderController")
+const { getAllOrders, getSingleOrder, updateOrderStatus, deleteOrder, updatePaymentStatus } = require("../../controller/admin/order/orderController")
 const isAuthenticated = require("../../middleware/isAuthenticated")
 const restrictTo = require("../../middleware/restrictTo")
 const catchAsync = require("../../services/catchAsync")
@@ -6,7 +6,10 @@ const catchAsync = require("../../services/catchAsync")
 const router = require("express").Router()
 
 router.route("/orders").get(isAuthenticated,restrictTo("admin"),catchAsync(getAllOrders))
+router.route("/orders/paymentstatus/:id").patch(isAuthenticated,restrictTo("admin"),catchAsync(updatePaymentStatus))
 router.route("/orders/:id").get(isAuthenticated,restrictTo("admin"),catchAsync(getSingleOrder)).patch(isAuthenticated,restrictTo("admin"),catchAsync(updateOrderStatus)).delete(isAuthenticated,restrictTo("admin"),catchAsync(deleteOrder))
+
+
 
 
 module.exports = router
